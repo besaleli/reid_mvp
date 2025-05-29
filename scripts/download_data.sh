103639d8c27415f009c51dba6dfa3f0c4acd5d3d
Raz Besaleli <besaleli@mac.mynetworksettings.com>
2025-05-29 17:21:14 -0400

    fix: add data mkdir in download data script

--- scripts/download_data.sh
+++ scripts/download_data.sh
@@ -1,2 +1,4 @@
+mkdir -p data
+
 curl -o data/video_1.mp4 https://uqnmqpvwlbpmdvutucia.supabase.co/storage/v1/object/public/test//access_control_part_1.mp4
 curl -o data/video_2.mp4 https://uqnmqpvwlbpmdvutucia.supabase.co/storage/v1/object/public/test//access_control_part_2.mp4
